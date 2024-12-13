4c80b06dcc95c3bf23b3ca009c284a5494855603
noahpessell <nwpessell@mail.com>
2024-12-13 04:39:52 -0500

    CtoFConverter Success

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,28 +1,27 @@
-import java.util.Scanner; // before the class at top of the file
+import java.util.Scanner;
 public class CtoFConverter
 {
-    // this code in main:
-    Scanner in = new Scanner(System.in);
-    double wage = 0;
-    String trash = ““; // use for bad input which will read as a String
-    boolean done = false;
-do
-    {
-        System.out.print(“Enter your hourly wage: “);
-        if(in.hasNextDouble()) // OK safe to read in a double
-        {
-            wage = in.nextDouble();
-            in.nextLine(); // clears the newline from the buffer
-            done = true; // we got a valid number so we can end the loop
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int degreesCelsius = 0;
+        int degreesFahrenheit = 0;
+        String trash = ""; // use for bad input which will read as a String
+        boolean done = false;
+        do {
+            System.out.print("Enter your degrees Celsius to be converted into Fahrenheit: "); //OUTPUT
+            if (in.hasNextInt()) // OK safe to read in an int
+            {
+                degreesCelsius = in.nextInt();
+                in.nextLine(); //clear input buffer
+                done = true; // we got a valid number so we can end the loop
+            } else {
+                trash = in.nextLine();
+                System.out.println("\n" + trash + " is not a degree of Celsius");
+                System.out.println("You have to enter a valid amount!");
+            }
         }
-        else
-        {
-            // Not a double so use nextLine() instead to read a String
-            trash = in.nextLine();
-            System.out.println(“\nYou said your wage was: “ + trash);
-            System.out.println(“You have to enter a valid amount!”);
-        }
-    }while(!done); // initially done is false so !done i.e. NOT FALSE, is true
-    // when done is true !done i.e. NOT TRUE is false
-
+            while (!done) ;
+            degreesFahrenheit = (((degreesCelsius * 9)/5)+32);
+            System.out.println(+ degreesCelsius + " degrees converted to Fahrenheit is: " + degreesFahrenheit + " degrees");
+    }
 }
--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -1,2 +1,41 @@
-public class RectangleInfo {
+import java.util.Scanner;
+public class RectangleInfo
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double itemPrice = 0; //listing out variables
+        double shipCost = 0;
+        double totalCost = 0;
+        String trash = ""; //trash is for invalid input
+        System.out.print("Enter your item price: "); //OUTPUT
+        if(in.hasNextDouble()) //checking if input is a double
+        {
+            itemPrice = in.nextDouble(); //input, set to variable
+            in.nextLine(); //clear input buffer
+            if(itemPrice >= 100) //shipping cost is 0 if item is more or equal to 100
+            {
+                shipCost = 0;
+                System.out.println("There is no shipping cost.");
+                totalCost = itemPrice + shipCost;
+                System.out.printf("Your total cost is: $" + totalCost);
+            }
+            else if(itemPrice < 100 && itemPrice >= 0) //shipping cost is 2% of item price if item is less than $100 and not negative
+            {
+                shipCost = itemPrice * .02;
+                System.out.printf("Your shipping cost is: $ %.2f" , shipCost);
+                totalCost = itemPrice + shipCost;
+                System.out.printf("%n" + "Your total cost is: $ %.2f" , totalCost); //%n is added to make a new line with SOUTprintf
+            }
+            else //if item price input is negative, it is an invalid number
+            {
+                System.out.println("Your input " + itemPrice + " is invalid. Please try again.");
+            }
+        }
+        else //if item price input was never a double, it is an invalid number
+        {
+            trash = in.nextLine();
+            System.out.println("Your input " + trash + " is invalid. Please try again.");
+        }
+    }
 }
