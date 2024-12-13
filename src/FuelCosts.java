81379197f704cb948616571c7edf4bfdf85ca592
noahpessell <nwpessell@mail.com>
2024-12-13 14:53:38 -0500

    Fuel Costs progress

--- src/FuelCosts.java
+++ src/FuelCosts.java
@@ -1,41 +1,77 @@
 import java.util.Scanner;
-public class FuelCosts
-{
-public static void main(String[] args)
-{
-    Scanner in = new Scanner(System.in);
-    double itemPrice = 0; //listing out variables
-    double shipCost = 0;
-    double totalCost = 0;
-    String trash = ""; //trash is for invalid input
-    System.out.print("Enter your item price: "); //OUTPUT
-    if(in.hasNextDouble()) //checking if input is a double
-    {
-        itemPrice = in.nextDouble(); //input, set to variable
-        in.nextLine(); //clear input buffer
-        if(itemPrice >= 100) //shipping cost is 0 if item is more or equal to 100
-        {
-            shipCost = 0;
-            System.out.println("There is no shipping cost.");
-            totalCost = itemPrice + shipCost;
-            System.out.printf("Your total cost is: $" + totalCost);
+public class FuelCosts {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double gasGallons = 0.00;
+        double milesPerGallon = 0.00;
+        double gallonGasPrice = 0.00;
+        String continueYN = "";
+        boolean done = false;
+        do {
+            System.out.print("Enter the number of gallons of gas that can go in your car's gas tank: "); //OUTPUT
+            if (in.hasNextDouble()) // OK safe to read in a double
+            {
+                gasGallons = in.nextDouble();
+                in.nextLine(); //clear input buffer
+                if (gasGallons < 0)
+                {
+                    System.out.println(+gasGallons + " is an invalid number of gallons. Try again? [YN] ");
+                    continueYN = in.nextLine();
+                    if (continueYN.equalsIgnoreCase("Y")) ;
+                    {
+                        done = true;
+                    }
+                }
+                else
+                {
+                    System.out.println("Continue [YN]: ");
+                    continueYN = in.nextLine();
+                    if (continueYN.equalsIgnoreCase("Y"))
+                    {
+                        done = true;
+                    }
+                    else
+                        {
+                            System.exit(0);
+                        }
+                }
+            }
         }
-        else if(itemPrice < 100 && itemPrice >= 0) //shipping cost is 2% of item price if item is less than $100 and not negative
-        {
-            shipCost = itemPrice * .02;
-            System.out.printf("Your shipping cost is: $ %.2f" , shipCost);
-            totalCost = itemPrice + shipCost;
-            System.out.printf("%n" + "Your total cost is: $ %.2f" , totalCost); //%n is added to make a new line with SOUTprintf
+        while (!done);
+        do {
+            System.out.print("Enter how many miles per gallon your car can travel: "); //OUTPUT
+            if (in.hasNextDouble()) // OK safe to read in an int
+            {
+                milesPerGallon = in.nextDouble();
+                in.nextLine(); //clear input buffer
+                System.out.println("Do you want to continue? [YN]: ");
+                continueYN = in.nextLine();
+                if (continueYN.equalsIgnoreCase("Y")) {
+                    done = true;
+                }
+                done = true; // we got a valid number so we can end the loop
+                if (continueYN.equalsIgnoreCase("Y")) {
+                    done = true;
+                }
+            } else {
+                System.out.println("\n You have ended the program!");
+            }
         }
-        else //if item price input is negative, it is an invalid number
-        {
-            System.out.println("Your input " + itemPrice + " is invalid. Please try again.");
+        while (!done);
+        do {
+            System.out.print("Enter the price of gas per gallon: "); //OUTPUT
+            if (in.hasNextDouble()) // OK safe to read in an int
+            {
+                gallonGasPrice = in.nextDouble();
+                in.nextLine(); //clear input buffer
+                done = true; // we got a valid number so we can end the loop
+                if (continueYN.equalsIgnoreCase("Y")) {
+                    done = true;
+                }
+            } else {
+                System.out.println("\n You have ended the program!");
+            }
         }
+        while (!done);
     }
-    else //if item price input was never a double, it is an invalid number
-    {
-        trash = in.nextLine();
-        System.out.println("Your input " + trash + " is invalid. Please try again.");
-    }
-}
 }
